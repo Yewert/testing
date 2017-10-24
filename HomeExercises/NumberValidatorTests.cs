4b6e841bf93c58a37037d7266d01f7c94a95383a
Yewert <blazer.andrew@gmail.com>
2017-10-24 23:03:41 +0500

    Homework

--- HomeExercises/NumberValidatorTests.cs
+++ HomeExercises/NumberValidatorTests.cs
@@ -7,27 +7,104 @@ namespace HomeExercises
 {
 	public class NumberValidatorTests
 	{
+		[TestCase(17, 2, false, "0",
+			ExpectedResult = true, TestName = "IntegerInBounds")]
+		[TestCase(17, 2, false, "0.0",
+			ExpectedResult = true, TestName = "FractionalInBounds")]
+		[TestCase(17, 2, false, "-0.0",
+			ExpectedResult = true, TestName = "NegativeFraction")]
+		[TestCase(17, 0, false, "1",
+			ExpectedResult = true, TestName = "IntegerInWithZeroScale")]
+		[TestCase(17, 0, false, "1.0",
+			ExpectedResult = false, TestName = "Fails_On_FractionalWithZeroScale")]
+		[TestCase(17, 2, true, "0.0",
+			ExpectedResult = true, TestName = "PositiveInOnlyPositiveMode")]
+		[TestCase(17, 2, true, "-0.0",
+			ExpectedResult = false, TestName = "Fails_On_NegativeInOnlyPositiveMode")]
+		[TestCase(17, 2, false, "0.000",
+			ExpectedResult = false, TestName = "Fails_On_FractionBiggerThanScale")]
+		[TestCase(3, 2, false, "0000",
+			ExpectedResult = false, TestName = "Fails_On_NumberBiggerThanPrecision")]
+		[TestCase(3, 2, false, "",
+			ExpectedResult = false, TestName = "Fails_On_Empty")]
+		[TestCase(3, 2, false, null,
+			ExpectedResult = false, TestName = "Fails_On_Null")]
+		[TestCase(3, 2, false, "+11.1",
+			ExpectedResult = false, TestName = "Fails_On_PositiveWithSignThatIsGreaterThanPrecision")]
+		[TestCase(3, 2, false, "-11.1",
+			ExpectedResult = false, TestName = "Fails_On_NegativeBecauseOfSing")]
+		[TestCase(3, 2, false, "kek",
+			ExpectedResult = false, TestName = "Fails_On_NonNumber")]
+		[TestCase(30, 2, false, "10,1",
+			ExpectedResult = true, TestName = "CommaSeparator")]
+		[TestCase(30, 2, false, "10.1.1",
+			ExpectedResult = false, TestName = "Fails_On_DoubleDot")]
+		[TestCase(30, 2, false, "  10.1  ",
+			ExpectedResult = false, TestName = "Fails_On_WhitespacesAround")]
+		[TestCase(30, 2, false, "10.",
+			ExpectedResult = false, TestName = "Fails_On_NoDigitAfterDot")]
+		public bool TestWithoutExceptions(int precision, int scale, bool unsigned, string value)
+		{
+			return new NumberValidator(precision, scale, unsigned).IsValidNumber(value);
+		}
+
+		
+		// В NUnit3 выпилили параметр ExpectedException из аттрибута TestCase :( 
+		
 		[Test]
-		public void Test()
+		public void Fails_On_ScaleGreaterThanPrecision()
+		{
+			Assert.Throws<ArgumentException>(() => new NumberValidator(1, 2, true));
+		}
+		
+		[Test]
+		public void Fails_On_ScaleEqualToPrecision()
+		{
+			Assert.Throws<ArgumentException>(() => new NumberValidator(2, 2, true));
+		}
+		
+		[Test]
+		public void Fails_On_NegativeScale()
+		{
+			Assert.Throws<ArgumentException>(() => new NumberValidator(1, -2, true));
+		}
+		
+		[Test]
+		public void Fails_On_Negativeprecision()
 		{
 			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
 		}
+		
+		
+//		//[Test]
+//		public void Test()
+//		{
+//			Assert.Multiple(() =>
+//			{
+//				Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
+//				Assert.Throws<ArgumentException>(() => new NumberValidator(1, 2, true));
+//				
+//				Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+//				//Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
+//				//Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+//
+//				Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+//				Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
+//				//Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+//				Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
+//				Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
+//				Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+//				//Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
+//				Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
+//				Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
+//				Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
+//				Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
+//				Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+//
+//				Assert.IsTrue(new NumberValidator(1, 0, false).IsValidNumber("1"));
+//				Assert.IsFalse(new NumberValidator(1, 0, false).IsValidNumber("-"));
+//			});
+//		}
 	}
 
 	public class NumberValidator
--- HomeExercises/ObjectComparison.cs
+++ HomeExercises/ObjectComparison.cs
@@ -5,28 +5,87 @@ namespace HomeExercises
 {
 	public class ObjectComparison
 	{
-		[Test]
-		[Description("Проверка текущего царя")]
-		[Category("ToRefactor")]
-		public void CheckCurrentTsar()
+//		[Test]
+//		[Description("Проверка текущего царя")]
+//		[Category("ToRefactor")]
+//		public void CheckCurrentTsar()
+//		{
+//			var actualTsar = TsarRegistry.GetCurrentTsar();
+//
+//			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+//				new Person("Vasili III of Russia", 28, 170, 60, null));
+//
+//			// Перепишите код на использование Fluent Assertions.
+//			actualTsar.Name.Should().Be(expectedTsar.Name);
+//			actualTsar.Age.Should().Be(expectedTsar.Age);
+//			actualTsar.Height.Should().Be(expectedTsar.Height);
+//			actualTsar.Weight.Should().Be(expectedTsar.Weight);
+//			
+//			actualTsar.Parent.Name.Should().Be(expectedTsar.Parent.Name);
+//			actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
+//			actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
+//			actualTsar.Parent.Parent.ShouldBeEquivalentTo(expectedTsar.Parent.Parent);
+//		}
+
+		private Person _actualTsar;
+		private Person _expectedTsar;
+
+		[SetUp]
+		public void LoadTsarsIntoTheMatrix()
 		{
-			var actualTsar = TsarRegistry.GetCurrentTsar();
-
-			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+			_actualTsar = TsarRegistry.GetCurrentTsar();
+			_expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 				new Person("Vasili III of Russia", 28, 170, 60, null));
-
-			// Перепишите код на использование Fluent Assertions.
-			Assert.AreEqual(actualTsar.Name, expectedTsar.Name);
-			Assert.AreEqual(actualTsar.Age, expectedTsar.Age);
-			Assert.AreEqual(actualTsar.Height, expectedTsar.Height);
-			Assert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-			Assert.AreEqual(expectedTsar.Parent.Name, actualTsar.Parent.Name);
-			Assert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-			Assert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-			Assert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
 		}
 
+		[Test]
+		public void NameMatch()
+		{
+			_actualTsar.Name.Should().Be(_expectedTsar.Name);
+		}
+
+		[Test]
+		public void AgeMatch()
+		{
+			_actualTsar.Age.Should().Be(_expectedTsar.Age);
+		}
+
+		[Test]
+		public void HeightMatch()
+		{
+			_actualTsar.Height.Should().Be(_expectedTsar.Height);
+		}
+
+		[Test]
+		public void WeightMatch()
+		{
+			_actualTsar.Weight.Should().Be(_expectedTsar.Weight);
+		}
+
+		[Test]
+		public void ParentsNameMatch()
+		{
+			_actualTsar.Parent.Name.Should().Be(_expectedTsar.Parent.Name);
+		}
+
+		[Test]
+		public void ParentsAgeMatch()
+		{
+			_actualTsar.Parent.Age.Should().Be(_expectedTsar.Parent.Age);
+		}
+
+		[Test]
+		public void ParentsHeightMacth()
+		{
+			_actualTsar.Parent.Height.Should().Be(_expectedTsar.Parent.Height);
+		}
+
+		[Test]
+		public void ParentsParentMatch()
+		{
+			_actualTsar.Parent.Parent.Should().Be(_expectedTsar.Parent.Parent);
+		}
+		
 		[Test]
 		[Description("Альтернативное решение. Какие у него недостатки?")]
 		public void CheckCurrentTsar_WithCustomEquality()
@@ -35,7 +94,15 @@ namespace HomeExercises
 			var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
 			new Person("Vasili III of Russia", 28, 170, 60, null));
 
-			// Какие недостатки у такого подхода? 
+			// Какие недостатки у такого подхода?
+			
+			// Корявый ассерт.
+			// Не понятно, какое именно из полей не правильное.
+			// Само сравнение менее читабильно в отличие от версии на Fluent Assertions,
+			// который читается почти как предложение.
+			
+			
+			// А что за прикол с ID в классе Person?
 			Assert.True(AreEqual(actualTsar, expectedTsar));
 
 		}
